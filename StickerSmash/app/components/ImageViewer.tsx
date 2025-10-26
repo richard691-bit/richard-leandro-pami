ae6f28d00bd35d6c19433b8d587b540834152a3e
richard leandro <negrelirichard@gmail.com>
2025-10-26 02:17:56 -0300

    abilitando o botão para extrair fotos fora da URL

--- StickerSmash/app/components/ImageViewer.tsx
+++ StickerSmash/app/components/ImageViewer.tsx
@@ -3,10 +3,14 @@ import { Image } from "expo-image";
 
 type Props = {
     imgSource: ImageSourcePropType;
+    selectedImage?: string;
 };
 
-export default function ImageViewer({ imgSource }: Props) {
-    return <Image source={imgSource} style={styles.image} />;
+export default function ImageViewer({ imgSource, selectedImage }: Props) {
+    const imageSorce = selectedImage ? { uri: selectedImage } : imgSource;
+
+    
+    return <Image source={imageSorce} style={styles.image}/>
 }
 
 const styles = StyleSheet.create({
