8ff6fd86ea5cfda70fa55286d4c6d0df25c7eee0
richard leandro <negrelirichard@gmail.com>
2025-10-30 20:11:01 -0300

    mechendo em algumas coisa

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -8,8 +8,8 @@ const PlaceholderImage = require ('../../assets/images/background-image.png');
 
 export default function Index() {
   const [selectedImage, setSlelectedImage] = useState <null | string | undefined>(undefined);
-
-
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+ 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
@@ -19,6 +19,7 @@ export default function Index() {
 
     if (!result.canceled) {
       setSlelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
     }else{
       alert('You did not select any image.');
     }
@@ -29,10 +30,14 @@ export default function Index() {
       <View style={styles.imageContainer}>
          <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
       </View>
+      {showAppOptions ?(
+        <View/>
+      ) : (
       <View style={styles.footerContainer}>
         <Button theme="primary" label="choose a photo" onPress={pickImageAsync}/>
-        <Button label="Use this photo" />
+        <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
       </View>
+      )}
     </View>
   );
 }
