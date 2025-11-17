c8a6f8c467ddaed992cce4b74f191a7ef501cc6c
richard leandro <negrelirichard@gmail.com>
2025-11-17 00:36:56 -0300

    capitulo 9 aplicativo concluido

--- StickerSmash/app/_layout.tsx
+++ StickerSmash/app/_layout.tsx
@@ -1,7 +1,10 @@
 import { Stack } from 'expo-router';
+import {StatusBar} from 'expo-status-bar';
+
 
 export default function RootLayout() {
   return (
+    <>
     <Stack>
         <Stack.Screen
         name="(tabs)"
@@ -12,5 +15,7 @@ export default function RootLayout() {
         />
         <Stack.Screen name ="+not-found" options={{}}/>
         </Stack>
+        <StatusBar style = "light"/>
+    </>
   );
 }
